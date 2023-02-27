48a6b112fbeaea2a7e37994d03473b6a9caff863
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-02-27 18:33:57 +0300

    duzenleme yapidli

--- posts.js
+++ posts.js
@@ -1,20 +1,20 @@
 let posts = [
-    { title: 'post 1', author: 'author 1', date: '2018' },
-    { title: 'post 2', author: 'author 2', date: '2019' },
-    { title: 'post 3', author: 'author 3', date: '2020' },
-    { title: 'post 4', author: 'author 4', date: '2021' },
+    { title: 'post 1', description: 'Post 1 içerigi', date: '2018' },
+    { title: 'post 2', description: 'Post 2 içerigi 2', date: '2019' },
+    { title: 'post 3', description: 'Post 3 içerigi 2', date: '2020' },
+    { title: 'post 4', description: 'Post 4 içerigi 2', date: '2021' },
 ]
 
 let listPosts = () => {
     posts.map(post => {
-        console.log(post.title, post.author, post.date)
+        console.log(post.title, post.description, post.date)
     })
 }
 
 let addPosts = () => {
     let promiseAddPost = new Promise((resolve, reject) => {
-        posts.push({ title: 'post 5', author: 'author 5', date: '2022' })
-        resolve('Kitap eklendi')
+        posts.push({ title: 'post 5', description: 'Post 5 icerigi', date: '2022' })
+        resolve('Post eklendi')
     })
 
     return promiseAddPost
