22f54b034d8a03ced982608c5d46c5dc0863bcf4
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-02-27 19:36:52 +0300

    dairenin cevresi ve alanı hesaplama

---
+++ circle/circle.js
@@ -0,0 +1,11 @@
+let circleArea  = (radius)=>{
+    return 2 * Math.PI * radius
+}
+
+let circleCircumference = (radius)=>{
+    return Math.PI* Math.sqrt(radius)
+}
+module.exports= {
+    circleArea,
+    circleCircumference
+}
